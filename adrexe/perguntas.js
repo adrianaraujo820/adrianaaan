b2a42c7bb3f0c833fa832160ff6fd63c13208b62
adrianaraujo820 <adrianaraujo820@gmail.com>
2024-11-25 16:00:43 -0300

    Add files via upload

---
+++ adrexe/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'animais',
+    'Eu sou um?',
+    'Cachorro caramelo'
+)
+
+criaCartao(
+    'Esporte',
+    'Handball não é?',
+    'ESporte'
+)
+
+criaCartao(
+    'Bea',
+    'A bea não pode?',
+    'sentir cheiro ruim'
+)
+
+criaCartao(
+    'eu',
+    'Eu não posso?',
+    'Sentir cheiro'
+)
